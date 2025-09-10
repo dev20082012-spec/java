30dd48eef6885afc7a9a8eb515b7139584bc83c8
dev20082012-spec <dev20082012@gmail.com>
2025-09-10 13:21:10 +0530

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -121,7 +121,44 @@ public class arrays {
 
 //         }
     //***************************************** remember ki initialisation for max and min ka***********************************
-  }                                 
+  
+
+
+
+                                                  /// [[{ lc 8-binary search}]]
+ 
+
+  public static int binarysearch(int numbers[],int key) {
+    int start = 0, end = numbers.length-1;  
+    while(start<=end) {
+        int mid = (start+end)/2;
+
+        // comparisons 
+        if(numbers[mid] == key) {   // found 
+            return mid;
+        }
+        if(numbers[mid]<key) {    // RIGHT
+            start = mid +1;
+
+        } else {  // LEFT 
+        end = mid-1;    
+        }
+    }
+return -1;
+  }                                                 
+   public static void main(String args[]) {
+    int numbers[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    int key = 25; 
+ int s = binarysearch(numbers, key);
+ if(s==-1) {
+    System.out.println("the data entered is out of range : "+s);
+ } else {
+    System.out.println("the index of the key is :"+s);
+ }
+
+}
+
+}                                 
 
 
 
