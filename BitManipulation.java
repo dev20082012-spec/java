60295c3fb0dc47ce61c3f5dd354ea90808d7a00d
dev20082012-spec <dev20082012@gmail.com>
2025-10-23 00:19:59 +0530

    Update BitManipulation.java

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,13 +1,42 @@
 import java.util.*;
 public class BitManipulation {
-    public static void oddOReven(int n ) {
-        int bitmask = 1;
+    public static void oddOReven(int n ) { // lc 10
+        int bitmask = 1; 
         if((n & bitmask) == 0) {
             System.out.println("the given no. is even");
         } else {
             System.out.println("the given no. is odd");
         }
     }
+    public static int getIthBIT(int n, int i) { // lc 11
+        int bitmask = 1<<i;
+        if((n & bitmask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    public static int setITHbit(int n, int i) { // lc 12
+        int bitmask = 1<<i;
+        return n | bitmask;
+    }
+    public static int clearITHbit(int n, int i) { // lc 13
+    int bitmask = ~(1<<i);
+    return n & bitmask;
+    }
+    public static int updateITHBIT(int n, int i, int newbit) {
+// Simplest way to done this.
+        // if(newbit == 0) {
+        //     return clearITHbit(n, i);
+        // } else {
+        //     return setITHbit(n, i);
+        // }
+
+// ANOTHER EASY WAY TO APPROACH THIS.        
+ n = clearITHbit(n, i);
+ int bitmask = newbit<<i;
+ return n | bitmask;    
+}
     public static void main(String args[]) {
         // lc 4 binary and 
         // System.out.println(5&6);
@@ -22,10 +51,23 @@ public class BitManipulation {
         // lc 9 binary RIGHT SHIFT 
         // System.out.println(6>>1);
         // lc 10 ODD OR EVEN
-Scanner sc = new Scanner(System.in);
-System.out.print(" enter your number of choice : ");
-int n = sc.nextInt();
-oddOReven(n);
+        Scanner sc = new Scanner(System.in);
+        // System.out.print(" enter your number of choice : ");
+        // int n = sc.nextInt();
+        // oddOReven(n);
+
+        // lc 11 GET ITH BIT
+        // System.out.println(getIthBIT(10, 1));
+        
+        // lc 12 SET ITH BIT
+        // System.out.println(setITHbit(10, 2));
+        
+        // lc 13 CLEAR ITH BIT
+        // System.out.println(clearITHbit(10, 1));
+        
+        // lc 14  
+        System.out.println(updateITHBIT(10, 2, 1));
+    
     }
     
 }
