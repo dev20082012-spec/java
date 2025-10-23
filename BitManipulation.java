c1e3095d52285846ba510b7396bc33ccc9da2a66
dev20082012-spec <dev20082012@gmail.com>
2025-10-23 23:25:59 +0530

    Update BitManipulation.java

--- BitManipulation.java
+++ BitManipulation.java
@@ -24,7 +24,7 @@ public class BitManipulation {
     int bitmask = ~(1<<i);
     return n & bitmask;
     }
-    public static int updateITHBIT(int n, int i, int newbit) {
+    public static int updateITHBIT(int n, int i, int newbit) { // lc 14
 // Simplest way to done this.
         // if(newbit == 0) {
         //     return clearITHbit(n, i);
@@ -36,6 +36,41 @@ public class BitManipulation {
  n = clearITHbit(n, i);
  int bitmask = newbit<<i;
  return n | bitmask;    
+}
+public static int clearIbit(int n, int i) { // lc 15
+    int bitmask = (~0) << i; // also knows ~0 = -1, can use -1 << i ; 
+    return n & bitmask;    
+
+}
+public static int clear_range0f_bits(int n, int i, int j) { // lc 16
+    int a = ((~0) << (j+1));
+    int b = (1<<i) -1;
+    int bitmask = a | b;
+    return n & bitmask;
+}
+public static boolean power_twoORnot(int n) { // lc 17
+    return (n&(n-1)) == 0;
+}
+public static int count_set_bits_number(int n) { // lc 18
+    int count = 0;
+    while(n>0) {
+        if((n&1) != 0) { // check our LSB
+            count++;
+        } 
+        n = n>>1;
+    }
+    return count;
+}
+public static int fast_EXPONENTIATION(int a, int n) { // lc 20
+    int ans = 1;
+    while(n>0) {
+        if((n&1) != 0) { // check LSB
+            ans = ans * a;
+        } 
+        a = a*a;
+        n = n>>1;
+    } 
+    return ans;
 }
     public static void main(String args[]) {
         // lc 4 binary and 
@@ -65,9 +100,23 @@ public class BitManipulation {
         // lc 13 CLEAR ITH BIT
         // System.out.println(clearITHbit(10, 1));
         
-        // lc 14  
-        System.out.println(updateITHBIT(10, 2, 1));
-    
+        // lc 14  update ITH bit
+        // System.out.println(updateITHBIT(10, 2, 1));
+        
+        // lc 15 clear last I bit
+        // System.out.println(clearIbit(15,2));     
+        
+        // lc 16 CLEAR RANGE OF BITS
+        // System.out.println(clear_range0f_bits(10, 2, 4)); 
+                
+        // lc 17 CHECK IF A NO. IS POWER OF 2 OR NOT
+        // System.out.println(power_twoORnot(16));
+                        
+        // lc 18 COUNT SET BITS IN NO.
+        // System.out.println(count_set_bits_number(15));
+                    
+        // lc 20 fast EXPONENTIATION code.
+        System.out.println(fast_EXPONENTIATION(3, 5));
     }
     
 }
